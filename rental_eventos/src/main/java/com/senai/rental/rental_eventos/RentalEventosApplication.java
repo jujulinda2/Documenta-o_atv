697e00c2ce83387ae65c89e49e2e4e7ed2fba2a3
Julia Pinheiro Bull <pet0155936@aluno.firjansenaisesi.com.br>
2026-08-25 11:02:03 -0300

    started project

---
+++ rental_eventos/src/main/java/com/senai/rental/rental_eventos/RentalEventosApplication.java
@@ -0,0 +1,15 @@
+package com.senai.rental.rental_eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalEventosApplication.class, args);
+		System.out.println( "Felippe é insuportável");
+	}
+
+
+}
---
+++ rental_eventos/src/test/java/com/senai/rental/rental_eventos/RentalEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.rental.rental_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
